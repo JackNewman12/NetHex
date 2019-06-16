8398766eb5accdeb805179498f0e4914895df875
Jack Newman <Jack.Newman12@gmail.com>
2019-06-16 22:40:40 +0930

    Minor formatting

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
+extern crate clap;
 extern crate hexplay;
 extern crate pnet;
-extern crate clap;
 
-use clap::{Arg, App, value_t};
+use clap::{value_t, App, Arg};
 
 use pnet::datalink::Channel::Ethernet;
-use pnet::datalink::{self, NetworkInterface, Config};
+use pnet::datalink::{self, Config, NetworkInterface};
 
 use std::io;
 use std::time::{Duration, Instant};
@@ -23,31 +23,40 @@ fn print_interfaces() {
 
 // Invoke as echo <interface name>
 fn main() {
-
     let matches = App::new("NetHex")
         .version("0.1.0")
         .author("Jack Newman")
         .about("A small utility for reading / writing directly to a network interface")
-        .arg(Arg::with_name("timeout")
-                 .short("t")
-                 .long("timeout")
-                 .takes_value(true)
-                 .help("Time before exiting the program"))
-        .arg(Arg::with_name("count")
-                 .short("c")
-                 .long("count")
-                 .takes_value(true)
-                 .help("Number of packet to receive before exiting")
-                 .default_value("-1"))
-        .arg(Arg::with_name("list")
-                 .short("l")
-                 .long("list")
-                 .help("List network interfaces"))
-        .arg(Arg::with_name("interface")
+        .arg(
+            Arg::with_name("timeout")
+                .short("t")
+                .long("timeout")
+                .takes_value(true)
+                .help("Time before exiting the program"),
+        )
+        .arg(
+            Arg::with_name("count")
+                .short("c")
+                .long("count")
+                .takes_value(true)
+                .help("Number of packet to receive before exiting")
+                .default_value("-1"),
+        )
+        .arg(
+            Arg::with_name("list")
+                .short("l")
+                .long("list")
+                .help("List network interfaces"),
+        )
+        .arg(
+            Arg::with_name("interface")
                 .help("The network interface to send/read from")
-                .required_unless("list"))
-        .arg(Arg::with_name("bytes")
-                .help("A hex string of raw bytes to send to the interface e.g. 11EE22FF"))
+                .required_unless("list"),
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .help("A hex string of raw bytes to send to the interface e.g. 11EE22FF"),
+        )
         .get_matches();
 
     // println!("{:?}", matches);
@@ -56,13 +65,14 @@ fn main() {
         std::process::exit(0);
     };
 
-    let rx_timeout = value_t!(matches, "timeout", u64).ok().map(|time| Duration::from_secs(time));
+    let rx_timeout = value_t!(matches, "timeout", u64)
+        .ok()
+        .map(|time| Duration::from_secs(time));
     let mut rx_countlimit = value_t!(matches, "count", i64).unwrap();
-
 
     // Grab the input interface. No error checking as clap will exit if it does not exist
     let interface_name = matches.value_of("interface").unwrap();
-    
+
     // Find the network interface with the provided name
     let interface_names_match = |iface: &NetworkInterface| iface.name == interface_name;
     let interfaces = datalink::interfaces();
@@ -74,12 +84,13 @@ fn main() {
 
     // Create a new channel, dealing with layer 2 packets
     let mut datalink_config = Config::default();
-    datalink_config.read_timeout = Some(std::time::Duration::new(0, 1e7 as u32)); // Set the timeout of the socket read to 10ms
+    // Set the timeout of the socket read to 10ms
+    datalink_config.read_timeout = Some(std::time::Duration::new(0, 1e7 as u32)); 
 
     let (mut tx, mut rx) = match datalink::channel(&interface, datalink_config) {
         Ok(Ethernet(tx, rx)) => (tx, rx),
         Ok(_) => panic!("Unhandled channel type"),
-        Err(e) => panic!("An error occurred when creating the datalink channel: {:?}", e),
+        Err(e) => panic!("Error while creating datalink channel: {:?}", e),
     };
 
     // Do the Tx part if the user specified bytes
@@ -88,8 +99,10 @@ fn main() {
         use hex::FromHex;
         let bytes = match Vec::from_hex(arg) {
             Ok(bytes) => bytes,
-            Err(e) => {println!("{}", e);
-                       std::process::exit(1);},
+            Err(e) => {
+                println!("{}", e);
+                std::process::exit(1);
+            }
         };
         println!("Sending bytes: {:X?}", bytes);
         tx.send_to(&bytes, None);
@@ -110,11 +123,12 @@ fn main() {
                 // Timeout errors are fine. Ignore.
             }
             Err(e) => {
-                // If an error occurs, we can handle it here
+                // If any other error occurs, we can handle it here
                 panic!("An error occurred while reading: {:?}", e);
             }
         }
         if let Some(rx_timeout) = rx_timeout {
+            // If there is a timeout enabled. Check it
             if now.elapsed() > rx_timeout {
                 std::process::exit(0);
             }
