5920d471655aa86ba569263ae36f6ffac56c9b2f
Jack Newman <Jack.Newman12@gmail.com>
2019-06-30 16:23:42 +0930

    Add Rx whitelist / blacklist filtering

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate hexplay;
 extern crate log;
 extern crate pbr;
 extern crate pnet;
+extern crate regex;
 
 use hex::FromHex;
 use log::Level::Info;
@@ -11,6 +12,7 @@ use log::{debug, error, info, log_enabled};
 use pbr::ProgressBar;
 use pnet::datalink::Channel::Ethernet;
 use pnet::datalink::{self, Config, NetworkInterface};
+use regex::RegexBuilder;
 use structopt::StructOpt;
 
 use std::io;
@@ -42,6 +44,14 @@ struct Opt {
     /// Time to receive for before exiting
     #[structopt(short = "t", long = "timeout")]
     rx_timeout: Option<u64>,
+
+    /// Only print Rx packets that match this regex filter
+    #[structopt(short = "f", long = "filter")]
+    rx_filter: Option<String>,
+
+    /// Only print Rx packets that do NOT match this regex filter
+    #[structopt(short = "b", long = "blacklist")]
+    rx_blacklist_filter: Option<String>,
 
     /// Number of packet to transmit
     #[structopt(short = "s", long = "send", default_value = "1")]
@@ -149,32 +159,70 @@ fn main() {
     }
 
     {
+        // Start of the Rx scope. First convert the users settings
         let rx_timeout = opt.rx_timeout.map(Duration::from_secs);
         let mut rx_countlimit = opt.rx_count;
+        let rx_filter = opt.rx_filter.map(|filter| {
+            RegexBuilder::new(&filter)
+                .case_insensitive(true)
+                .multi_line(true)
+                .dot_matches_new_line(true)
+                .build()
+                .expect("Invalid Regex")
+        });
+        debug!("Whitelist is: {:?}", rx_filter);
+        let rx_blacklist_filter = opt.rx_blacklist_filter.map(|filter| {
+            RegexBuilder::new(&filter)
+                .case_insensitive(true)
+                .multi_line(true)
+                .dot_matches_new_line(true)
+                .build()
+                .expect("Invalid Regex")
+        });
+        debug!("Blacklist is: {:?}", rx_blacklist_filter);
         let wg = wg.clone();
-        // Now do the Rx part
+
+        // Now spawn the thread for performing the Rx'ing
         thread::spawn(move || {
+            // If you dont want it to print Rx packets. 
+            // Kill the thread via -c 0
             let now = Instant::now();
             while rx_countlimit != 0 {
                 match rx.next() {
                     Ok(packet) => {
+                        // Format the packet into a nice hex format
                         use hexplay::HexViewBuilder;
                         let view = HexViewBuilder::new(packet).row_width(16).finish();
-                        // Dont think this makes sense to be part of the logger.
-                        // If you dont want it to print. Kill the thread via -c 0
+
+                        // Match the whitelist if set
+                        if let Some(rx_filter) = &rx_filter {
+                            if !rx_filter.is_match(&format!("{}", view)) {
+                                continue;
+                            }
+                        }
+                        // Do not match the blackist if set
+                        if let Some(rx_blacklist_filter) = &rx_blacklist_filter {
+                            if rx_blacklist_filter.is_match(&format!("{}", view)) {
+                                continue;
+                            }
+                        }
+                        // Print the packet
                         println!("Recv Packet\n{}", view);
                         rx_countlimit -= 1;
                     }
                     Err(ref e) if e.kind() == io::ErrorKind::TimedOut => {
-                        // Timeout errors are fine. Ignore.
+                        // Timeout errors are fine
+                        // Pass
                     }
                     Err(e) => {
                         // If any other error occurs, we can handle it here
                         panic!("An error occurred while reading: {:?}", e);
                     }
                 }
+
+                // If there is a timeout enabled. Check it
+                // TDB if this moves to it's own thread so it works for both Tx and Rx
                 if let Some(rx_timeout) = rx_timeout {
-                    // If there is a timeout enabled. Check it
                     if now.elapsed() > rx_timeout {
                         debug!("Rx time limit reached!");
                         std::process::exit(0);
